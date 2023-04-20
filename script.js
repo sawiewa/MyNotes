d1f85cb0839de885596678863a8fb0760aa19d36
Ewa Sawicka <esawicka.25@gmail.com>
2023-04-20 21:18:53 +0200

    Dodanie notatki, otwarcie i zamknięcie formularza

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const noteArea = document.querySelector('.note-area');
 const notePanel = document.querySelector('.note-panel');
 const category = document.querySelector('#category');
 const textarea = document.querySelector('#text');
-const error = document.querySelector('error');
+const error = document.querySelector('.error');
 let selectedValue;
 let cardID = 0; //każda karta bedzie miała unikalne id
 
@@ -18,10 +18,47 @@ const openPanel = () => {
 
 const closePanel = () => {
 	notePanel.style.display = 'none';
-	//error.style.visibility = 'hidden';
+	error.style.visibility = 'hidden'; //czysci error w przypadku gdy wystąpił podczas dodawania formularza
 	textarea.value = ''; //czyści po kliknięciu anuluj, żeby przy ponownym otwarciu pole było puste
 	category.selectedIndex = 0; // wybiera domyślny indeks, --wybierz kategorie--
 };
 
+const addNote = () => {
+	if (
+		textarea.value !== '' &&
+		category.options[category.selectedIndex].value !== '0' //dostajemy się do konkretnej wartości value wybranej opcji
+	) {
+		createNote(); // jeżeli wszystkie pola uzupełnione to będzie wywoływana funkcja która stworzy notatkę i tu ją wywołamy
+		error.style.visibility = 'hidden';
+	} else {
+		error.style.visibility = 'visible';
+	}
+};
+
+const createNote = () => {
+	const newNote = document.createElement('div');
+	newNote.classList.add('note');
+	newNote.setAttribute('id', cardID);
+	newNote.innerHTML = `<div class="note-header">
+	<h3 class="note-title">${selectedValue}</h3>
+	<button class="delete-note"><i class="fas fa-times icon"></i></button>
+
+</div>
+<div class="note-body">
+	${textarea.value}
+</div>`;
+
+	noteArea.appendChild(newNote);
+	cardID++; //zmieniamy id o 1
+	textarea.value = ''; //czyści po kliknięciu anuluj, żeby przy ponownym otwarciu pole było puste
+	category.selectedIndex = 0;
+	closePanel();
+};
+
+const selectValue = () => {
+	selectedValue = category.options[category.selectedIndex].text;
+};
+
 addBtn.addEventListener('click', openPanel);
 cancelBtn.addEventListener('click', closePanel);
+addSave.addEventListener('click', addNote);
